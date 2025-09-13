67af0837e5129abd2ab617a3b3377bafe4e2b880
inuzuki1113 <daweiha413@gmail.com>
2025-09-14 02:36:07 +0900

    Update network.c

--- source/network.c
+++ source/network.c
@@ -1,8 +1,8 @@
 #include "network.h"
 #include <3ds.h>
 #include <string.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 void networkInit() {
     httpcInit(0);
@@ -12,7 +12,7 @@ void networkExit() {
     httpcExit();
 }
 
-// ニコニコ軽量HTML検索取得（簡易版）
+// ニコニコ検索ページからログイン不要動画URLとタイトルを取得（簡易版）
 int searchNicoVideos(const char* keyword, char videoURLs[][256], char titles[][128], int maxCount) {
     char url[512];
     snprintf(url, sizeof(url), "https://sp.nicovideo.jp/search/%s", keyword);
@@ -31,19 +31,24 @@ int searchNicoVideos(const char* keyword, char videoURLs[][256], char titles[][1
     u32 size = 0;
     httpcGetDownloadSizeState(&context, &size);
     char* buffer = malloc(size + 1);
+    if (!buffer) {
+        httpcCloseContext(&context);
+        return 0;
+    }
     memset(buffer, 0, size + 1);
     httpcReceiveData(&context, buffer, size, NULL);
 
     int count = 0;
     char* p = buffer;
     while (count < maxCount && (p = strstr(p, "<a href=\"/watch/")) != NULL) {
-        p += 9; // /watch/ まで進める
+        p += 9; // "/watch/"の直後
         strncpy(videoURLs[count], "/watch/", 7);
         int i = 7;
         while (*p != '"' && i < 255) videoURLs[count][i++] = *p++;
         videoURLs[count][i] = 0;
 
-        strcpy(titles[count], "動画タイトル");
+        // タイトル取得簡易版
+        strcpy(titles[count], "ログイン不要動画");
 
         count++;
     }
