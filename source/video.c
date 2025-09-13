6c880051dbe13e892fefb98ea9b88844100e7644
inuzuki1113 <daweiha413@gmail.com>
2025-09-14 02:32:54 +0900

    Create video.c

---
+++ source/video.c
@@ -0,0 +1,15 @@
+#include "video.h"
+#include <3ds.h>
+
+void videoInit() {
+    // 上画面初期化
+}
+
+void videoExit() {
+    // 上画面終了処理
+}
+
+void videoUpdate() {
+    // CTRMP4ストリーミング再生（144p固定）
+    // ここでネットワークバッファを読み込みつつ再生
+}
